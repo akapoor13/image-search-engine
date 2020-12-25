6f1d1d4abae6badcb41ad8666a7ec658f4e5a3bc
Anshul Kapoor <akapoor13@outlook.com>
2020-12-24 21:17:46 -0500

    undo

--- app.py
+++ app.py
@@ -14,20 +14,6 @@ app.config.suppress_callback_exceptions = True
 app.layout = app_layout()
 callback(app)
 
-if os.path.exists('image_files') or os.path.exists('assets/image_files'):
-    import shutil
-    try:
-        shutil.rmtree('image_files')
-        print('removed image_files')
-    except:
-        print('unsuccessful image_files')
-
-    try:
-        shutil.rmtree('assets/image_files')
-        print('removed assets')
-    except:
-        print('break')
-
 if __name__ == '__main__':
     app.run_server(debug=True)
     # app.run_server()
